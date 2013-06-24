0012ab2b010a7b9c5fe7ca922ce6c82c5289becc
Alexandre <alexandre@vidal.adv.br>
2013-06-24 16:44:09 -0300

    alteracao do teste

--- src/test/java/br/com/caelum/agiletickets/controllers/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/controllers/SessaoTest.java
@@ -2,20 +2,27 @@ package br.com.caelum.agiletickets.controllers;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.agiletickets.models.Sessao;
 
 public class SessaoTest {
 
+	private Sessao sessao;
+	
+	@Before
+	public void inicializa(){
+		this.sessao = new Sessao();
+		sessao.setTotalIngressos(8);
+		System.out.println("A anotacao @Before é chamada antes dos demais");
+	}
 	/**
 	 * @param args
 	 */
 	@Test
-	public void reservaTotalDisponivel() {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(8);
-		Assert.assertEquals(true, sessao.podeReservar(8));
+	public void deveTestarAReservaDoTotalDisponivel() {
+		Assert.assertEquals(true, this.sessao.podeReservar(8));
 	}
 
 }
